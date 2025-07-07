9041671945cc35ee0219fab7787bba3fbc8585bb
bibhuxsharmaa <victusbibhu@gmail.com>
2025-07-07 13:59:04 +0530

    nice

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-// MOVING CURSOR WITH THE HELP OF JS
+// FOR SCROLLING ANIMATION
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
 
-var videocon = document.querySelector("#video-container");
+
+
+
+
+
+// MOVING CURSOR WITH THE HELP OF JS
+function videoconAnimation (){
+    var videocon = document.querySelector("#video-container");
 var playbtn = document.querySelector("#play");
 videocon.addEventListener("mouseenter",function(){
     gsap.to(playbtn,{
@@ -28,3 +39,23 @@ videocon.addEventListener("mousemove",function(dets){
 
 
 })
+}
+videoconAnimation();
+
+//LOADING ANIMATION
+
+gsap.from("#page1 h1",{
+    y:100,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    stagger:0.3
+});
+
+gsap.from("#page1 #video-container",{
+    scale:0.9,
+    opacity:0,
+    delay:1.5,
+    duration:0.5,
+    stagger:0.3
+});
